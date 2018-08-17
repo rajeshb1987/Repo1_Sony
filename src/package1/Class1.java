1d711ba934b62a4b17dbe4d53d7e2c7a1492ac56
rajeshb1987 <rajeshbommidi@gmail.com>
2018-08-18 00:02:35 +0530

    Commit from GUI to MyNewBranch1

--- src/package1/Class1.java
+++ src/package1/Class1.java
@@ -8,6 +8,7 @@ public class Class1 {
 		System.out.println("This is first line");
 		System.out.println("Added this new line.This is second line");
 		System.out.println("Added after new branch");
+		System.out.println("To test GUI");
 		
 
 	}
