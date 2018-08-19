02e80e8fe247097f4f89a13e8f04c7148b2d5ee4
rajeshb1987 <rajeshbommidi@gmail.com>
2018-08-19 20:43:33 +0530

    Commiting to local branch MyNewBranch1 for testing mail notification from GITHUB

--- src/package1/Class1.java
+++ src/package1/Class1.java
@@ -9,6 +9,7 @@ public class Class1 {
 		System.out.println("Added this new line.This is second line");
 		System.out.println("Added after new branch");
 		System.out.println("To test GUI");
+		System.out.println("Line added to test mail sent from GITHUB");
 		
 
 	}
